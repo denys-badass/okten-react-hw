7b2c7f23d6e9b2d395a08b58595940aa95a4e8a8
Denys Podobrii <denys.podobrii@gmail.com>
2025-03-26 18:09:52 +0200

    task-98uhQGhaN add service for fetch comments

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,14 +1,19 @@
 import {IUser} from "../models/IUser.ts";
 import {IPost} from "../models/IPost.ts";
+import {IComment} from "../models/IComment.ts";
 
 const usersUrl: string = import.meta.env.VITE_API_BASE_URL + '/users';
-const postsUrl: string = import.meta.env.VITE_API_BASE_URL + '/posts'
+const postsUrl: string = import.meta.env.VITE_API_BASE_URL + '/posts';
+const commentsUrl: string = import.meta.env.VITE_API_BASE_URL + '/comments'
 
 export const getUsers = async (): Promise<IUser[]> => {
     return await fetch(usersUrl).then(response => response.json());
 }
 
 export const getPostsByUserId = async (userId: number): Promise<IPost[]> => {
-    console.log(`API request for user ${userId}`);
     return await fetch(postsUrl + `?userId=${userId}`).then(response => response.json());
+}
+
+export const getCommentsByPostId = async (postId: number): Promise<IComment[]> => {
+    return await fetch(commentsUrl + `?postId=${postId}`).then(response => response.json());
 }
