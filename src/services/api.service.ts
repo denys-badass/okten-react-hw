29ae80ae7d720d0a2c3125a8d864e01dff1e4006
Denys Podobrii <denys.podobrii@gmail.com>
2025-03-27 21:12:05 +0200

    task-q2faxfR_posts change base url and services

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,19 +1,18 @@
-import {IUser} from "../models/IUser.ts";
-import {IPost} from "../models/IPost.ts";
-import {IComment} from "../models/IComment.ts";
+import {IFetchedComments, IFetchedPosts, IFetchedUsers} from "../models/IFetched.ts";
 
-const usersUrl: string = import.meta.env.VITE_API_BASE_URL + '/users';
-const postsUrl: string = import.meta.env.VITE_API_BASE_URL + '/posts';
-const commentsUrl: string = import.meta.env.VITE_API_BASE_URL + '/comments'
+const baseUrl = new URL(import.meta.env.VITE_API_BASE_URL)
 
-export const getUsers = async (): Promise<IUser[]> => {
-    return await fetch(usersUrl).then(response => response.json());
+export const getUsers = async (): Promise<IFetchedUsers> => {
+    const endpoint = new URL('/users', baseUrl);
+    return await fetch(endpoint).then(response => response.json());
 }
 
-export const getPostsByUserId = async (userId: number): Promise<IPost[]> => {
-    return await fetch(postsUrl + `?userId=${userId}`).then(response => response.json());
+export const getPostsByUserId = async (userId: number): Promise<IFetchedPosts> => {
+    const endpoint = new URL(`/users/${userId}/posts`, baseUrl);
+    return await fetch(endpoint).then(response => response.json());
 }
 
-export const getCommentsByPostId = async (postId: number): Promise<IComment[]> => {
-    return await fetch(commentsUrl + `?postId=${postId}`).then(response => response.json());
+export const getCommentsByPostId = async (postId: number): Promise<IFetchedComments> => {
+    const endpoint = new URL(`/comments/post/${postId}`, baseUrl);
+    return await fetch(endpoint).then(response => response.json());
 }
