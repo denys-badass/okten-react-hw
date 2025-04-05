d9e229fc48f89971a1ae087cf4d4001021ba7e76
Denys Podobrii <denys.podobrii@gmail.com>
2025-04-05 09:28:13 +0300

    task-knZgVIwvqd done

--- src/pages/CartsPage.tsx
+++ src/pages/CartsPage.tsx
@@ -1,12 +1,21 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import UserCarts from "../components/user-carts/UserCarts.tsx";
 
 const CartsPage = () => {
     const {userId} = useParams();
+    const navigate = useNavigate();
+
+    const backNavigateHandle = () => {
+        navigate('/');
+    }
 
     return (
         <div>
-            {userId && <UserCarts userId={userId}/>}
+            {userId &&
+                <div>
+                    <button onClick={backNavigateHandle}><i className="fa-solid fa-arrow-left"></i> Back</button>
+                    <UserCarts userId={userId}/>
+                </div>}
         </div>
     );
 };
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,9 +1,8 @@
 import {createBrowserRouter} from "react-router-dom";
-import MainLayout from "../layouts/MainLayout.tsx";
 import CartsPage from "../pages/CartsPage.tsx";
+import UsersPage from "../pages/UsersPage.tsx";
 
 export const routes = createBrowserRouter([
-    {path: '/', element: <MainLayout/>, children: [
-            {path: 'user/:userId/carts/', element: <CartsPage/>}
-        ]}
+    {path: '/', element: <UsersPage/>},
+    {path: 'user/:userId/carts/', element: <CartsPage/>}
 ]);
