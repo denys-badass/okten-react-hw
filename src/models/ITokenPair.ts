83985c8b67bc213a380a1b32fd9ae809eda1b275
Denys Podobrii <denys.podobrii@gmail.com>
2025-04-14 11:00:04 +0300

    task-1 add models

---
+++ src/models/ITokenPair.ts
@@ -0,0 +1,5 @@
+// Model for refresh token response
+export interface ITokenPair {
+    accessToken: string;
+    refreshToken: string;
+}
---
+++ src/models/IUserWithTokens.ts
@@ -0,0 +1,12 @@
+// Models for User response after authentication with tokens
+export interface IUserWithTokens {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    image: string;
+    accessToken: string;
+    refreshToken: string;
+}
